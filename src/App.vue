e932d504f044bfaa042c4ff47a323f2ebc5c68df
dfrankmv <dfrankmg@gmail.com>
2023-05-06 01:38:21 +0000

    MainForm created

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,24 @@
 <template>
-  <el-button>hola mundos</el-button>
+    <h4>Scalper</h4>
+    <main-form :model="data.cmd" />
+    <pre><code>{{ JSON.stringify(data) }}</code></pre>
 </template>
+
+<script>
+import MainForm from './components/MainForm.vue';
+
+export default {
+    components: {
+        MainForm,
+    },
+    data() { return {
+        data: {
+            cmd: {
+                amount: null,
+                profit: null,
+            },
+            orders: [],
+        }
+    }}
+}
+</script>
